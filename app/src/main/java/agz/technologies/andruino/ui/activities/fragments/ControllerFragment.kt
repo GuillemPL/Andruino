618b4667730b26a8f1d713b382cdadf5be47fe54
piqueras.lopez.guillem <piqueras.lopez.guillem@alumnat.copernic.cat>
2021-05-20 21:01:27 +0200

    controlador firebase esritura

--- app/src/main/java/agz/technologies/andruino/ui/activities/fragments/ControllerFragment.kt
+++ app/src/main/java/agz/technologies/andruino/ui/activities/fragments/ControllerFragment.kt
@@ -1,7 +1,7 @@
 package agz.technologies.andruino.ui.activities.fragments
 
 import agz.technologies.andruino.R
-import agz.technologies.andruino.databinding.FragmentCameraBinding
+import agz.technologies.andruino.databinding.FragmentControllerBinding
 import android.Manifest
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
@@ -9,6 +9,7 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.View.SCROLLBARS_OUTSIDE_OVERLAY
 import android.view.ViewGroup
@@ -18,11 +19,14 @@ import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class ControllerFragment : Fragment() {
-    private lateinit var binding: FragmentCameraBinding
+    private lateinit var binding: FragmentControllerBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +39,7 @@ class ControllerFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentCameraBinding.bind(view)
+        binding = FragmentControllerBinding.bind(view)
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.RECORD_AUDIO
@@ -50,7 +54,46 @@ class ControllerFragment : Fragment() {
             ActivityCompat.requestPermissions(requireActivity(), permissions, 0)
         }
         webViewSetup()
+        binding.up.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_DOWN == event.action){
+                controlador("1")
+            }else if(MotionEvent.ACTION_UP == event.action){
+                controlador("0")
+            }
+            false
+        }
 
+
+        binding.down.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_DOWN == event.action){
+                controlador("4")
+            }else if(MotionEvent.ACTION_UP == event.action){
+                controlador("0")
+            }
+            false
+        }
+        binding.left.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_DOWN == event.action){
+                controlador("3")
+            }else if(MotionEvent.ACTION_UP == event.action){
+                controlador("0")
+            }
+            false
+        }
+        binding.right.setOnTouchListener { v, event ->
+            if (MotionEvent.ACTION_DOWN == event.action){
+                controlador("2")
+            }else if(MotionEvent.ACTION_UP == event.action){
+                controlador("0")
+            }
+            false
+        }
+
+    }
+
+    private fun controlador(data: String) {
+        FirebaseFirestore.getInstance().collection("user").document(FirebaseAuth.getInstance().currentUser?.email.toString()).set(
+            hashMapOf("controller" to data))
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -69,9 +112,11 @@ class ControllerFragment : Fragment() {
             settings.mediaPlaybackRequiresUserGesture = false
 
             settings.userAgentString = newUA
-            loadUrl("https://zoom.us/signin")
+            loadUrl("https://www.youtube.com/watch?v=8SEQaB7BrLg&ab_channel=MightExos")
         }
     }
+
+
 
     override fun onResume() {
         super.onResume()
